0b217546f39ef1980147030635771b7668012f96
antoniomattar <antoniomattar123@icloud.com>
2023-01-07 21:39:29 +0100

    updated my profile pic

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,13 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+        <img
+              src={logo}
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
