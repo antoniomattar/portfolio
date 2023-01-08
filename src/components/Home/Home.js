c7464a5b92b34d320e30eb7d1372c4d2dda04950
antoniomattar <antoniomattar123@icloud.com>
2023-01-08 11:21:16 +0100

    forgot a space

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'm
-                <strong className="main-name">Antonio Mattar</strong>
+                <strong className="main-name"> Antonio Mattar</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
