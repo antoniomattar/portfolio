ad71018b96b68cb696ea2a185a2a11802601f20a
antoniomattar <antoniomattar123@icloud.com>
2023-04-22 10:20:03 +0200

    added new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import f1 from "../../Assets/Projects/f1.png";
+import LBL from "../../Assets/Projects/LBL.png";
 
 function Projects() {
   return (
@@ -24,6 +25,14 @@ function Projects() {
               description="Sports streaming website to watch F1 ."
               ghLink="https://github.com/antoniomattar/f1-web"
               demoLink="https://f1-web-amber.vercel.app/"
+            />
+            <ProjectCard
+              imgPath={LBL}
+              isBlog={false}
+              title="Lebanese Basketball Website"
+              description="Find everything related to the LBL (Standings, games, stats)."
+              ghLink="https://github.com/antoniomattar/leb-basketball-site-next"
+              demoLink="https://leb-basketball-site-next.vercel.app"
             />
           </Col>
 
